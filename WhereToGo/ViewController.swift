9c6aad65723e3c58e4de1b66dada2117fa4c0276
89Explorer <leral3@naver.com>
2024-08-01 05:34:43 +0900

    Initial Commit

---
+++ WhereToGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhereToGo
+//
+//  Created by 권정근 on 8/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
